d38fbf8eca12fbae02f72a6ed0d85d2ce16bac29
Ram Soyao <r.soyao@live.com>
2018-05-16 15:51:52 -0400

    inserted the deleting URL function as well as the updated url page. Still having a problem with editing the shortURL.... but we working on itgit add .

--- expressEx/express_server.js
+++ expressEx/express_server.js
@@ -11,28 +11,66 @@ function generateRandomString() {
     input += possible.charAt(Math.floor(Math.random() * possible.length));
   return input;
 }
-console.log(generateRandomString());
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   "3rf9Df": "http://facebook.com"
 };
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Index - URL List
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Create a Post
 app.post("/urls", (req, res) => {
   var a = req.body.longURL;
   var b = generateRandomString();
   urlDatabase[b] = a;
   console.log(urlDatabase);  // debug statement to see POST parameters
-  res.redirect('/urls' + b)        // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/" + b)
+});
+
+//Edit  a post
+// app.get("urls/:id/edit", (req, res) => {
+//   const id = req.params.id;
+//   const long = urlDatabase[id];
+
+//   if (long) {
+//     res.render("urls_show", { id: id, long: long});
+//     res.redirect(`/urls/${id}`);
+//   } else {
+//     res.redirect("/urls");
+//   }
+// });
+
+//Update
+app.post("urls/:short", (req, res) => {
+ // let longURL = req.body.longURL
+ // urlDatabase[req.params.id] = longURL
+ let short = req.params.short;
+ let long = urlDatabase[short];
+ urlDatabase[short] = req.body.input;
+ if (long) {
+  res.redirect('/urls')
+ }
+});
+
+// Delete a post
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  const long = urlDatabase[id];
+
+  if (long) {
+    delete urlDatabase[id];
+  }
+
+  res.redirect("/urls");
 });
 
 app.get("/urls/new", (req, res) => {
@@ -63,8 +101,5 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.listen(PORT,() => {
-    if(err) {
-    throw new Error('Failed to start server on localhost:8080!')
-  }
   console.log(`Example app listening on port ${PORT}!`);
 });
